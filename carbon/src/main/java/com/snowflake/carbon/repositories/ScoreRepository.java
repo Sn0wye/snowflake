3d7481170107bc6d11aa1257975049066dfa215a
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-06-02 10:19:22 -0300

    fix: change ScoreRepository ID type from String to UUID

--- carbon/src/main/java/com/snowflake/carbon/repositories/ScoreRepository.java
+++ carbon/src/main/java/com/snowflake/carbon/repositories/ScoreRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.snowflake.carbon.entities.Score;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface ScoreRepository extends JpaRepository<Score, String> {
+public interface ScoreRepository extends JpaRepository<Score, UUID> {
     Optional<Score> findByUserId(String userId);
 }
