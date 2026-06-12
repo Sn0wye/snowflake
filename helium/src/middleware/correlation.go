7ecb069ae13da075fe0cf15c317e1c6b8e3b87b5
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-06-11 21:22:53 -0300

    feat(helium): add correlation ID middleware

---
+++ helium/src/middleware/correlation.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github.com/getsnowflake/snowflake/helium/pkg/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+const CorrelationIDKey = "correlation_id"
+
+func CorrelationMiddleware(log *logger.Logger) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Get("X-Correlation-ID")
+		if id == "" {
+			id = uuid.New().String()
+		}
+		c.Locals(CorrelationIDKey, id)
+		c.Set("X-Correlation-ID", id)
+		log.NewContext(c, zap.String("correlation_id", id))
+		return c.Next()
+	}
+}
+
+func GetCorrelationID(c *fiber.Ctx) string {
+	id, _ := c.Locals(CorrelationIDKey).(string)
+	return id
+}
